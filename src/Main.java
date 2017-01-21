b1824a40787d07f835bab1dd8d51c228789b2d0d
ioioj5 <peng_du2007@qq.com>
2017-01-22 00:05:52 +0800

    add simple spider

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 import normal.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 //import java.util.ArrayList;
@@ -9,11 +15,18 @@ import java.util.ArrayList;
 //import java.util.Iterator;
 
 public class Main {
-	public static void main(String[] args) {
-		CollectionTest ct = new CollectionTest();
-		ArrayList<String> al = ct.ArrayListTest();
-		//System.out.println(al);
-		ct.loopOne(al);
+    public static void main(String[] args) {
+        String url = "http://www.baidu.com";
+        // 定义一个字符串用来存储网页内容
+        String result = sendGet(url);
+        System.out.println(result);
+
+
+
+//		CollectionTest ct = new CollectionTest();
+//		ArrayList<String> al = ct.ArrayListTest();
+//		//System.out.println(al);
+//		ct.loopOne(al);
 
 //        Person p = new Person();
 //        p.setName("王五");
@@ -29,7 +42,7 @@ public class Main {
 //		Woman wm = new Woman();
 //		System.out.println("我是: " + wm.getName() + ", 今年 " + wm.getAge() + " 岁, 性别 " + wm.getSex());
 
-		// 集合
+        // 集合
 
 //		System.out.println("# ArrayList");
 //		// 1. ArrayList
@@ -83,5 +96,40 @@ public class Main {
 //			System.out.println(book);
 //		}
 
-	}
+    }
+    static String sendGet(String url){
+        // 定义一个缓冲字符输入流
+        BufferedReader in = null;
+        String result = "";
+
+        try {
+            // 将string转成url对象
+            URL realUrl = new URL(url);
+            // 初始化一个链接到那个url的连接
+            URLConnection connection = realUrl.openConnection();
+            // 开始实际的连接
+            connection.connect();
+            // 初始化 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            // 用来临时存储抓取到的每一行的数据
+            String line;
+            while ((line = in.readLine()) != null) {
+                // 遍历抓取到的每一行并将其存储到result里面
+                result += line + "\n";
+            }
+        } catch (Exception e) {
+
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        } finally { // 使用finally来关闭输入流
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
 }
